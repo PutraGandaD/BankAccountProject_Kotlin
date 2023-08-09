8b03ae5d40057ae99c3ce27b4c022871b7f63d32
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-08-09 12:20:03 +0700

    Finished part 2
    In this part 2, i have added a lot of changes.
    
    1. There's a function to simulate a real bank process, such as :
       - Deposit
       - Withdraw
       And for each type of account (whether it's debit, checking, or credit),
       the implementation of both Deposit and Withdraw function is different.
    
       For example, in credit accounts, i have to make sure that the accountBalance
       value is always going to be 0 or in negative numbers in order to simulate a
       real credit bank account process.
    
    2. I added a lot of comments and notes to train my code documentation skills.
       I also added a test code to test each of the function i created, so i directly
       can test the function that i just added.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,26 @@ fun main() {
     println("2. Credit account")
     println("3. Checking account")
 
-    var accountType: String = ""
-    var userChoice: Int = 0
+    var accountType: String = "" // account type
+    var userChoice: Int = 0 // user choice
+
+    val money: Int = (0..1000).random() // generate random money
+    var output: Int = 0 // temporary variable for testing printed output
+
+    // Always work for Debit and Checking account
+    //var accountBalance: Int = (1..1000).random()
+
+    /*
+     This is for Credit account.
+     You need to make sure the account balance is always 0
+     or in negative numbers in order to "simulate" real
+     credit bank account. That's why i created separate code
+     for this one.
+
+     Use code below to simulate credit bank account balance,
+     especially for creditDeposit function.
+     */
+    var accountBalance = -(0..1000).random()
 
     while (accountType == "") {
         userChoice = (1..5).random()
@@ -19,4 +37,77 @@ fun main() {
         }
     }
     println("You have created a $accountType account.")
+    println("Your current balance is $accountBalance dollars")
+
+    // Function to withdraw money from Checking and credit bank accounts
+    fun withdraw(amount: Int) : Int {
+        accountBalance -= amount
+        println("You successfully withdrew $amount dollars")
+        println("Your current balance is $accountBalance dollars")
+        return amount
+    }
+
+    // Function to withdraw money from debit
+    fun debitWithdraw(amount: Int) : Int {
+        println("Amount of money you requested for withdraw = $amount dollars")
+        if (accountBalance == 0) {
+            println("Your current balance is $accountBalance. Withdraw cannot be processed")
+            return accountBalance
+        } else {
+            if (amount > accountBalance) {
+                println("Your account balance is less than amount you requested for withdraw. Withdraw cannot be processed")
+                return 0
+            } else {
+                return withdraw(amount)
+            }
+        }
+    }
+
+    // Function to deposit for checking and debit account
+    fun deposit(amount: Int) : Int {
+        accountBalance += amount
+        println("You deposited $amount dollars to your account")
+        println("Your current account balance is $accountBalance dollars")
+        return amount
+    }
+
+    // Function to deposit for credit account
+    fun creditDeposit(amount: Int) : Int {
+        println("The amount of money you request to deposit is $amount dollars")
+        if (accountBalance == 0) {
+            println("You don't need to deposit anything in order to pay off the account since it has already been paid off")
+            return accountBalance
+        } else {
+            if (accountBalance + amount > 0) {
+                println("Deposit failed, you tried to pay off an amount greater than the credit balance. The checking balance is $accountBalance dollars.")
+                return 0
+            } else {
+                if (amount == -accountBalance) {
+                    accountBalance = 0
+                    println("You have paid off this account!")
+                    return amount
+                } else {
+                    return deposit(amount)
+                }
+            }
+        }
+    }
+
+    // TEST : Withdraw money from Checking and credit bank accounts
+    //withdraw(money)
+
+    // TEST : Withdraw money from debit
+    //output = debitWithdraw(money)
+    //println("You've successfully withdrew $output dollars from your debit card.")
+
+    // TEST : Deposit money for checking and debit account
+    //deposit(money)
+
+    // TEST : Deposit money for credit account
+    /*
+     Please pay attention to accountBalance value (check the code above, scroll a bit).
+     I made a separate accountBalance code where it's going to always generate
+     in negative numbers to simulate a real credit bank account deposit.
+     */
+    creditDeposit(money)
 }
